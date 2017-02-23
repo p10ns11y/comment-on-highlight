2687cc20c0453d00f572e80f36157ee7c871abb2
p10ns11y <sathyam.peram@gmail.com>
2017-02-24 00:45:26 +0100

    Add root component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import LandingPage from './LandingPage';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <LandingPage />
+      </div>
+    );
+  }
+}
+
+export default App;
